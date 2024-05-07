f9d68a3c5c4d029d3c3d41b31ccceeaabb6e4862
Mikssxed <eksilonxd@gmail.com>
2024-05-07 12:57:25 +0200

    WA-9: added state

--- src/components/AccountListItem.tsx
+++ src/components/AccountListItem.tsx
@@ -32,7 +32,7 @@ export default function AccountListItem({ account }: Props) {
   const { address, balance } = account;
   const { activeAccount, setActiveAccount, deleteAccount } = useAccountsStore();
   const { ask, alert } = useModalStore();
-  const { authorize } = useAuthStore();
+  const { authorize, state } = useAuthStore();
   const [visibleTooltip, setVisibleTooltip] = useState<boolean>(false);
   const navigation = useNavigation<NavigationProp>();
 
