e7d31eecbc0a2989631cb6f4a598c5889f5f1381
Mikssxed <eksilonxd@gmail.com>
2024-07-03 11:54:29 +0200

    WA-149: Added gradient accents

--- src/screens/onboarding/OnboardingFinishScreen.tsx
+++ src/screens/onboarding/OnboardingFinishScreen.tsx
@@ -9,7 +9,8 @@ import { useOnboardingStore } from "@/store";
 import { resetNavigationStack } from "@/utils";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect } from "react";
-import { View } from "react-native";
+import { Dimensions, Image, View } from "react-native";
+const blob = require("../../../assets/blob.png");
 
 type OnboardingFinishScreenProps = NativeStackScreenProps<
   OnboardingParamList,
@@ -20,6 +21,7 @@ export default function OnboardingFinishScreen({
   navigation,
 }: OnboardingFinishScreenProps) {
   const onboardingStore = useOnboardingStore();
+  const { width, height } = Dimensions.get("window");
 
   useEffect(() => {
     resetNavigationStack(navigation);
@@ -39,6 +41,33 @@ export default function OnboardingFinishScreen({
         title="Confirm"
         onPress={() => onboardingStore.finishOnboarding()}
       />
+      <View // This view is here to make the blob image appear behind on android, elevation is not supported on Image
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+          elevation: -1,
+        }}
+      >
+        <Image
+          style={{
+            position: "absolute",
+            width: width,
+            top: 0,
+            right: 100,
+            transform: [
+              { translateX: (width + 50) / 2 },
+              { translateY: -(width + 50) / 2 },
+              { scale: 1.5 },
+            ],
+          }}
+          source={blob}
+          resizeMode="contain"
+        />
+      </View>
     </SafeLayoutBottom>
   );
 }
--- src/screens/onboarding/OnboardingWelcomeScreen.tsx
+++ src/screens/onboarding/OnboardingWelcomeScreen.tsx
@@ -1,9 +1,11 @@
 import { Headline, Paragraph, SafeLayoutBottom } from "@/components";
-import { AddWallet } from "@/screens/newWallet";
 import { APP_NAME } from "@/const";
-import { View } from "react-native";
+import { AddWallet } from "@/screens/newWallet";
+import { Dimensions, Image, View } from "react-native";
+const blob = require("../../../assets/blob.png");
 
 export default function OnboardingWelcomeScreen() {
+  const { width, height } = Dimensions.get("window");
   return (
     <SafeLayoutBottom>
       <View>
@@ -15,6 +17,53 @@ export default function OnboardingWelcomeScreen() {
       </View>
 
       <AddWallet />
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          transform: [
+            { rotate: "160deg" },
+            { scale: 0.8 },
+            { translateX: -width / 7 },
+          ],
+          zIndex: -1,
+          elevation: -1,
+        }}
+      >
+        <Image
+          style={{
+            position: "absolute",
+            top: height / 12,
+            left: width / 5,
+            width: width,
+            transform: [
+              { translateX: -(width + 50) / 2 },
+              { translateY: -(width + 50) / 2 },
+              { scale: 1.5 },
+            ],
+          }}
+          source={blob}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            position: "absolute",
+            bottom: height / 12,
+            right: width / 5,
+            width: width,
+            transform: [
+              { translateX: (width + 50) / 2 },
+              { translateY: (width + 50) / 2 },
+              { scale: 1.5 },
+            ],
+          }}
+          source={blob}
+          resizeMode="contain"
+        />
+      </View>
     </SafeLayoutBottom>
   );
 }
