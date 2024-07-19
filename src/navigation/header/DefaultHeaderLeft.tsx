8f6209d799bb9ae2136010978bd80983dcd96d1e
Mikssxed <eksilonxd@gmail.com>
2024-07-19 12:55:13 +0200

    cr changes

--- src/navigation/header/DefaultHeaderLeft.tsx
+++ src/navigation/header/DefaultHeaderLeft.tsx
@@ -2,10 +2,8 @@ import { Colors } from "@/styles";
 import { useNavigation } from "@react-navigation/native";
 import { ArrowLeft } from "iconsax-react-native";
 import { Platform, Pressable } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function DefaultHeaderLeft() {
-  const insets = useSafeAreaInsets();
   const navigation = useNavigation();
   return Platform.OS === "ios" ? (
     <Pressable
