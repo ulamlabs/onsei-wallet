f3d34fc55e787c688889b99086a9dedc97bad2e1
Mikssxed <eksilonxd@gmail.com>
2024-07-19 12:53:55 +0200

    cr changes

--- src/navigation/header/DefaultHeaderLeft.tsx
+++ src/navigation/header/DefaultHeaderLeft.tsx
@@ -1,12 +1,22 @@
+import { Colors } from "@/styles";
 import { useNavigation } from "@react-navigation/native";
 import { ArrowLeft } from "iconsax-react-native";
-import { Pressable } from "react-native";
+import { Platform, Pressable } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function DefaultHeaderLeft() {
+  const insets = useSafeAreaInsets();
   const navigation = useNavigation();
-  return (
-    <Pressable onPress={() => navigation.goBack()} style={{ marginRight: 14 }}>
-      <ArrowLeft />
+  return Platform.OS === "ios" ? (
+    <Pressable
+      onPress={() => navigation.goBack()}
+      style={{
+        marginRight: 24,
+      }}
+    >
+      <ArrowLeft color={Colors.text} />
     </Pressable>
+  ) : (
+    <></>
   );
 }
