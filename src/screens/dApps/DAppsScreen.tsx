8a09c20a9ee56819235ca0b3b5c5fe57d16d2163
Damian Bednarz <damian.bednarz@ulam.io>
2024-09-17 02:59:36 +0200

    index update

--- src/screens/dApps/DAppsScreen.tsx
+++ src/screens/dApps/DAppsScreen.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { WebView } from "react-native-webview";
+import Browser from "./Browser";
 
 export default function DApps() {
   return (
-    <WebView
-      source={{ uri: "http://localhost:5173/" }}
-      style={{ flex: 1 }} // The URL to load in the WebView
-    />
+    <Browser/>
   );
 }
