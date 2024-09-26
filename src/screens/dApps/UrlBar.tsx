ed4fc92434e3d2cfea5a14a64b315be61048e244
Damian Bednarz <damian.bednarz@ulam.io>
2024-09-26 14:06:52 +0200

    fix lint

--- src/screens/dApps/UrlBar.tsx
+++ src/screens/dApps/UrlBar.tsx
@@ -5,15 +5,15 @@ import React from "react";
 
 export default function UrlBar() {
   const { url, setUrl } = useDAppsStore();
-  const searchInput = useInputState({initialValue:url});
+  const searchInput = useInputState({ initialValue: url });
 
   return (
-    <Row style={{ paddingTop:24, paddingBottom:12, }}>
+    <Row style={{ paddingTop: 24, paddingBottom: 12 }}>
       <TextInput
         {...searchInput}
         keyboardType="web-search"
         onSubmitEditing={() => setUrl(searchInput.value)}
-        containerStyle={{width:"100%"}}
+        containerStyle={{ width: "100%" }}
       />
     </Row>
   );
