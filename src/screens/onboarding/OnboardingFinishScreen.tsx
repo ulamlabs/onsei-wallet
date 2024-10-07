ab42430b4713f0d3e6f6b277e25423762b55117e
Wojciech Masłowski <112573426+Mikssxed@users.noreply.github.com>
2024-10-07 10:35:42 +0200

    WA-194: Add Terms&Conditions and Privacy Policy (#112)
    * WA-194: added links to policy
    
    * WA-194: disable buttons properly

--- src/screens/onboarding/OnboardingFinishScreen.tsx
+++ src/screens/onboarding/OnboardingFinishScreen.tsx
@@ -1,9 +1,17 @@
-import { Loader, Row, SafeLayoutBottom } from "@/components";
+import {
+  Checkbox,
+  Paragraph,
+  PrimaryButton,
+  ResultHeader,
+  SafeLayoutBottom,
+  TextLink,
+} from "@/components";
 import { OnboardingParamList } from "@/navigation/OnboardingNavigation";
 import { useOnboardingStore } from "@/store";
 import { resetNavigationStack } from "@/utils";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
 type OnboardingFinishScreenProps = NativeStackScreenProps<
   OnboardingParamList,
@@ -14,6 +22,7 @@ export default function OnboardingFinishScreen({
   navigation,
 }: OnboardingFinishScreenProps) {
   const onboardingStore = useOnboardingStore();
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     resetNavigationStack(navigation);
@@ -25,9 +34,48 @@ export default function OnboardingFinishScreen({
 
   return (
     <SafeLayoutBottom>
-      <Row style={{ justifyContent: "center", flex: 1 }}>
-        <Loader size="large" />
-      </Row>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ResultHeader
+          type="Success"
+          header="You're all done!"
+          description="You can now fully enjoy your wallet."
+        />
+      </View>
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 20,
+            justifyContent: "center",
+            paddingRight: 42, // checkbox width + gap
+            marginBottom: 32,
+          }}
+        >
+          <Checkbox setChecked={setChecked} checked={checked} />
+          <View style={{ alignItems: "center" }}>
+            <Paragraph>I have read and agree to the</Paragraph>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
+            >
+              <TextLink
+                url="https://www.onseiwallet.io/terms-and-conditions"
+                text="Terms of Use"
+              />
+              <Paragraph>and</Paragraph>
+              <TextLink
+                url="https://www.onseiwallet.io/privacy-policy"
+                text="Privacy Policy"
+              />
+            </View>
+          </View>
+        </View>
+        <PrimaryButton
+          title="Confirm"
+          onPress={() => onboardingStore.finishOnboarding()}
+          disabled={!checked}
+        />
+      </View>
     </SafeLayoutBottom>
   );
 }
