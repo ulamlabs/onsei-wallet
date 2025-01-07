7c8014d8bd659b7dee2de85218023ab0de2d3b24
Mateusz Sznura <mateusz.sznura@ulam.io>
2025-01-07 09:50:46 +0100

    add screen header

--- src/screens/bridgeAggregator/BridgeAggregator.tsx
+++ src/screens/bridgeAggregator/BridgeAggregator.tsx
@@ -1,10 +1,25 @@
+import { SafeLayout } from "@/components";
+import DashboardHeader from "@/navigation/header/DashboardHeader";
+import DefaultHeaderTitle from "@/navigation/header/DefaultHeaderTitle";
 import { NavigatorParamsList } from "@/types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import React from "react";
 
-type DashboardProps = NativeStackScreenProps<NavigatorParamsList, "Bridge">;
+type BridgeAggregatorProps = NativeStackScreenProps<
+  NavigatorParamsList,
+  "Bridge"
+>;
 
-export default function BridgeAggregator({ navigation }: DashboardProps) {
-  return <></>;
+export default function BridgeAggregator({
+  navigation,
+}: BridgeAggregatorProps) {
+  return (
+    <>
+      <DashboardHeader>
+        <DefaultHeaderTitle title="Onsei bridge aggregator" />
+      </DashboardHeader>
+      <SafeLayout style={{ paddingBottom: 80 }}></SafeLayout>
+    </>
+  );
 }
