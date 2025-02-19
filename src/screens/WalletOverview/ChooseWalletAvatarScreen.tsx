30bebe78e11d7c91f752ef97bbe733aa3032c1ce
Doryski <dominik.rycharski20@gmail.com>
2025-02-19 07:54:39 +0100

    fix: position and functionality of Save button

--- src/screens/WalletOverview/ChooseWalletAvatarScreen.tsx
+++ src/screens/WalletOverview/ChooseWalletAvatarScreen.tsx
@@ -3,18 +3,25 @@ import { NavigatorParamsList } from "@/types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { CloseIcon, PrimaryButton, SafeLayout, Text } from "@/components";
-import { TouchableOpacity, View, StyleSheet, FlatList } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import Avatar from "@/components/Avatar";
 import { Colors } from "@/styles";
 import { Account, useAccountsStore } from "@/store";
 import { useState } from "react";
 import { NFTGalleryCard } from "@/screens/nftsGallery/NFTsGalleryList";
 import { FontWeights, FontSizes } from "@/styles";
-import { useNFTs } from "@/modules/nfts/api";
+import { NFTInfo, useNFTs } from "@/modules/nfts/api";
 import { getNFTImage } from "../nftsGallery/utils";
 import CenteredLoader from "@/components/CenteredLoader";
 import ErrorNFTsGallery from "../nftsGallery/ErrorNFTsGallery";
 import EmptyNFTsGallery from "../nftsGallery/EmptyNFTsGallery";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type ChooseWalletAvatarScreenProps = NativeStackScreenProps<
   NavigatorParamsList,
@@ -22,6 +29,7 @@ type ChooseWalletAvatarScreenProps = NativeStackScreenProps<
 >;
 
 const CARDS_GAP = 16;
+const AVATAR_SIZE = 144;
 
 const tabs = [
   "NFT Collection",
@@ -38,66 +46,25 @@ export default function ChooseWalletAvatarScreen({
   const [draftAvatar, setDraftAvatar] = useState<Account["avatar"]>(
     account?.avatar,
   );
+  console.log("draftAvatar", draftAvatar);
+  console.log("account?.avatar", account?.avatar);
+  const inset = useSafeAreaInsets();
   const { setAvatar } = useAccountsStore();
   const [activeTab, setActiveTab] = useState<Tab>("NFT Collection");
-  const { nfts, listStates } = useNFTs();
 
   const handleSavePress = () => {
     setAvatar(account?.address, draftAvatar);
     navigation.goBack();
   };
 
+  const handleNFTSelect = (nft: NFTInfo) => {
+    setDraftAvatar(getNFTImage(nft));
+  };
+
   const renderTabContent = () => {
     switch (activeTab) {
       case "NFT Collection":
-        return (
-          <View>
-            {(listStates.isLoading ||
-              listStates.isEmpty ||
-              listStates.isError) && (
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  paddingTop: 50,
-                }}
-              >
-                {listStates.isLoading && <CenteredLoader size="big" />}
-                {listStates.isError && <ErrorNFTsGallery />}
-                {listStates.isEmpty && <EmptyNFTsGallery />}
-              </View>
-            )}
-            {listStates.isSuccess && (
-              <FlatList
-                key="nfts"
-                data={nfts.data}
-                numColumns={2}
-                scrollEnabled={false}
-                contentContainerStyle={styles.flatList}
-                keyExtractor={(item) => item.tokenId}
-                renderItem={({ item }) => (
-                  <NFTGalleryCard
-                    nft={item}
-                    onPress={() => setDraftAvatar(getNFTImage(item))}
-                  />
-                )}
-                columnWrapperStyle={styles.columnWrapper}
-              />
-            )}
-            <PrimaryButton
-              title="Save"
-              onPress={handleSavePress}
-              disabled={draftAvatar === account?.avatar}
-              style={{
-                position: "absolute",
-                top: 360,
-                left: 0,
-                right: 0,
-              }}
-            />
-          </View>
-        );
+        return <NFTCollectionTab onNFTSelect={handleNFTSelect} />;
       //   case "Emojis":
       //     return <View />;
       default:
@@ -111,70 +78,151 @@ export default function ChooseWalletAvatarScreen({
   };
 
   return (
-    <SafeLayout subScreen style={{ position: "relative" }}>
-      <View>
-        {account && (
-          <TouchableOpacity
-            style={{ position: "relative", alignItems: "center" }}
-            onPress={handleAvatarPress}
-          >
-            <Avatar
-              src={draftAvatar || account?.avatar}
-              name={account?.name}
-              size={144}
-              rounded
-            />
-            {(draftAvatar || account?.avatar) && (
-              <View
-                style={{
-                  position: "absolute",
-                  bottom: -8,
-                  right: 144 / 2 + 24 + 16,
-                  borderRadius: 300,
-                  backgroundColor: Colors.tokenBoxBackground,
-                  borderWidth: 3,
-                  borderColor: Colors.background,
-                  width: 38,
-                  height: 38,
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <CloseIcon size={18} color={Colors.text} />
-              </View>
-            )}
-          </TouchableOpacity>
-        )}
-
-        <View style={styles.tabContainer}>
-          {tabs.map((tab) => (
+    <SafeLayout subScreen staticView style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.content}>
+          {account && (
             <TouchableOpacity
-              key={tab}
-              style={[
-                styles.tabButton,
-                activeTab === tab && styles.activeTabButton,
-              ]}
-              onPress={() => setActiveTab(tab)}
+              style={{ position: "relative", alignItems: "center" }}
+              onPress={handleAvatarPress}
             >
-              <Text
-                style={[
-                  styles.tabText,
-                  activeTab === tab && styles.activeTabText,
-                ]}
-              >
-                {tab}
-              </Text>
+              <Avatar
+                src={draftAvatar}
+                name={account?.name}
+                size={AVATAR_SIZE}
+                rounded
+              />
+              {draftAvatar && (
+                <View
+                  style={{
+                    position: "absolute",
+                    bottom: -8,
+                    right: AVATAR_SIZE / 2 + 24 + 16,
+                    borderRadius: 300,
+                    backgroundColor: Colors.tokenBoxBackground,
+                    borderWidth: 3,
+                    borderColor: Colors.background,
+                    width: 38,
+                    height: 38,
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <CloseIcon size={18} color={Colors.text} />
+                </View>
+              )}
             </TouchableOpacity>
-          ))}
-        </View>
+          )}
 
-        <View style={styles.contentContainer}>{renderTabContent()}</View>
+          <View style={styles.tabContainer}>
+            {tabs.map((tab) => (
+              <TouchableOpacity
+                key={tab}
+                style={[
+                  styles.tabButton,
+                  activeTab === tab && styles.activeTabButton,
+                ]}
+                onPress={() => setActiveTab(tab)}
+              >
+                <Text
+                  style={[
+                    styles.tabText,
+                    activeTab === tab && styles.activeTabText,
+                  ]}
+                >
+                  {tab}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+
+          <View style={styles.contentContainer}>{renderTabContent()}</View>
+        </View>
+      </ScrollView>
+
+      <View
+        style={[
+          styles.buttonWrapper,
+          {
+            bottom: 144 + inset.bottom,
+          },
+        ]}
+      >
+        <PrimaryButton
+          title="Save"
+          onPress={handleSavePress}
+          disabled={draftAvatar === account?.avatar}
+        />
       </View>
     </SafeLayout>
   );
 }
 
+type NFTCollectionTabProps = {
+  onNFTSelect: (nft: NFTInfo) => void;
+};
+
+function NFTCollectionTab({ onNFTSelect }: NFTCollectionTabProps) {
+  const { nfts, listStates } = useNFTs();
+
+  return (
+    <View>
+      {(listStates.isLoading || listStates.isEmpty || listStates.isError) && (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: 50,
+          }}
+        >
+          {listStates.isLoading && <CenteredLoader size="big" />}
+          {listStates.isError && <ErrorNFTsGallery />}
+          {listStates.isEmpty && <EmptyNFTsGallery />}
+        </View>
+      )}
+      {listStates.isSuccess && (
+        <FlatList
+          key="nfts"
+          data={nfts.data}
+          numColumns={2}
+          scrollEnabled={false}
+          contentContainerStyle={styles.flatList}
+          keyExtractor={(item) => item.tokenId}
+          renderItem={({ item }) => (
+            <NFTGalleryCard nft={item} onPress={() => onNFTSelect(item)} />
+          )}
+          columnWrapperStyle={styles.columnWrapper}
+        />
+      )}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  content: {
+    paddingBottom: 120,
+  },
+  buttonWrapper: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    paddingHorizontal: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+  },
   tabContainer: {
     flexDirection: "row",
     marginTop: 36,
